e4573dda259ed1c63f5575427ec452818619f652
emilygaj <emilygaj@users.noreply.github.com>
2022-04-27 20:10:57 +0000

    deploy: 7019c3efb1b33808da9358d6ef1bc44e43a8d639

--- js/main.js
+++ js/main.js
@@ -57,3 +57,21 @@ function newElement() {
     };
   }
 }
+
+var modal = document.getElementsByClassName("modal");
+var btn = document.getElementsByClassName("sources");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
