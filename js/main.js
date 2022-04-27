a20a9a17144c1490cd514992035c69fb10307a14
emilygaj <emilygaj@users.noreply.github.com>
2022-04-27 23:02:57 +0000

    deploy: ba1c3e30bbca153faa257127096b2459e7fdb64d

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-var modal = document.getElementsByClassName("modal");
-var btn = document.getElementsByTagName("button");
+// Make the modal -- Code from W3 Schools "How to Create a Modal Box"
+var modal = document.getElementById("MyModal");
+
+//Get the button that opens the modal
+var btn = document.getElementById("source-icon");
+
+// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-function openModal() {
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-btn.addEventListener("click", function openModal() {
-  console.log("element clicked");
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
